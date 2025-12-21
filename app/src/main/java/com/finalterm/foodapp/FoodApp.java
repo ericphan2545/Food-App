66a0ac156c45b4c13e50fbfeccf418c7f8326c5b
ericphan2545 <ericphan2545@gmail.com>
2025-12-21 21:55:08 +0700

    App v1.0 (#36)
    * add recipe-api-service file
    
    * v1.0

---
+++ app/src/main/java/com/finalterm/foodapp/FoodApp.java
@@ -0,0 +1,15 @@
+package com.finalterm.foodapp;
+
+import android.app.Application;
+
+public class FoodApp extends Application {
+    
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        
+        // Apply saved theme on app start
+        SettingsActivity.applyTheme(this);
+    }
+}
+
